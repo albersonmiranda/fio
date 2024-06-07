d30772cd31c8ef0fb00f00dc0cb549c7586f8b26
Alberson Miranda <albersonmiranda@hotmail.com>
2024-06-07 18:00:56 -0300

    fix leontief inverse

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,19 +1,22 @@
-use extendr_api::prelude::*;
+// module imports
+mod multipliers;
+
 use faer;
 use faer::prelude::SpSolver;
+use extendr_api::prelude::*;
 
 #[extendr]
-/// Calculates technical coefficients matrix to R.
+/// Computes technical coefficients matrix to R.
 /// @param intermediate_transactions A nxn matrix of intermediate transactions.
 /// @param total_production A 1xn vector of total production.
 /// @return A nxn matrix of technical coefficients, known as A matrix.
 /// @examples
 /// intermediate_transactions <- matrix(c(1, 2, 3, 4), nrow = 2)
 /// total_production <- c(10, 20)
-/// tec_coeff(intermediate_transactions, total_production)
+/// tech_coeff(intermediate_transactions, total_production)
 /// @export
 
-fn tec_coeff(
+fn compute_tech_coeff(
   intermediate_transactions: Vec<f64>,
   total_production: Vec<f64>,
 ) -> RArray<f64, [usize;2]> {
@@ -22,43 +25,43 @@ fn tec_coeff(
   let n = (intermediate_transactions.len() as f64).sqrt() as usize;
 
   // divide each entry of intermediate_transactions by each column of total_production
-  let mut tec_coeff = intermediate_transactions.clone();
+  let mut tech_coeff = intermediate_transactions.clone();
   for i in 0..n {
     for j in 0..n {
-      tec_coeff[i * n + j] = tec_coeff[i * n + j] / total_production[i];
+      tech_coeff[i * n + j] = tech_coeff[i * n + j] / total_production[i];
     }
   }
 
   // convert to R matrix
-  let tec_coeff_r = RArray::new_matrix(n, n, |r, c| tec_coeff[r + c * n]);
+  let tech_coeff_r = RArray::new_matrix(n, n, |r, c| tech_coeff[r + c * n]);
 
-  tec_coeff_r
+  tech_coeff_r
 }
 
 #[extendr]
-/// Calculates Leontief inverse matrix to R.
-/// @param tec_coeff A nxn matrix of technical coefficients.
+/// Computes Leontief inverse matrix to R.
+/// @param tech_coeff A nxn matrix of technical coefficients.
 /// @return A nxn matrix of Leontief inverse.
 /// @examples
-/// tec_coeff <- matrix(c(0.1, 0.2, 0.3, 0.4), nrow = 2)
-/// leontief_inverse(tec_coeff)
+/// tech_coeff <- matrix(c(0.1, 0.2, 0.3, 0.4), nrow = 2)
+/// leontief_inverse(tech_coeff)
 /// @export
 
-fn leontief_inverse(tec_coeff: Vec<f64>) -> RArray<f64, [usize;2]> {
+fn compute_leontief_inverse(tech_coeff: Vec<f64>) -> RArray<f64, [usize;2]> {
 
   // get dimensions
-  let n = (tec_coeff.len() as f64).sqrt() as usize;
+  let n = (tech_coeff.len() as f64).sqrt() as usize;
 
   // create faer matrix
-  let tec_coeff_matrix = faer::Mat::from_fn(n, n, |r, c| (r + c) as f64);
+  let tech_coeff_matrix = faer::Mat::from_fn(n, n, |row, col| tech_coeff[col * n + row]);
 
   // calculate Leontief matrix
   let identity_matrix: faer::Mat<f64> = faer::Mat::identity(n, n);
-  let leontief_matrix = identity_matrix.clone() - tec_coeff_matrix;
+  let leontief_matrix = identity_matrix.clone() - tech_coeff_matrix;
 
   // calculate Leontief inverse
-  let leontief_lu = leontief_matrix.partial_piv_lu();
-  let leontief_inverse = leontief_lu.solve(&identity_matrix);
+  let lu = leontief_matrix.partial_piv_lu();
+  let leontief_inverse = lu.solve(&identity_matrix);
 
   // convert to R matrix
   let leontief_inverse_r = RArray::new_matrix(n, n, |r, c| leontief_inverse[(r, c)]);
@@ -70,6 +73,7 @@ fn leontief_inverse(tec_coeff: Vec<f64>) -> RArray<f64, [usize;2]> {
 // See corresponding C code in `entrypoint.c`.
 extendr_module! {
   mod fio;
-  fn tec_coeff;
-  fn leontief_inverse;
+  use multipliers;
+  fn compute_tech_coeff;
+  fn compute_leontief_inverse;
 }
