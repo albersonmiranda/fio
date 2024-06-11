8bf510df4b51fa37feb6ebfc7411c14f777c9c2b
Alberson Miranda <45690517+albersonmiranda@users.noreply.github.com>
2024-06-11 14:25:03 +0000

    setup Rust in codespaces

---
+++ .devcontainer/setup_rust.sh
@@ -0,0 +1,23 @@
+## update and install some things we should probably have
+apt-get update
+apt-get install -y \
+  curl \
+  git \
+  gnupg2 \
+  jq \
+  sudo \
+  zsh \
+  vim \
+  build-essential \
+  openssl
+
+## Install rustup and common components
+curl https://sh.rustup.rs -sSf | sh -s -- -y 
+rustup install nightly
+rustup component add rustfmt
+rustup component add rustfmt --toolchain nightly
+rustup component add clippy 
+rustup component add clippy --toolchain nightly
+
+cargo install cargo-expand
+cargo install cargo-edit
